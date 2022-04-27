7444dfcf9a9182b6aa50fe7d10f90c564fb21a31
Meri <mgogi13@freeuni.edu.ge>
2022-04-27 17:24:58 +0400

    feat: greeting works according to the tutorial

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import HelloWorld from './HelloWorld';
+import configureStore from '../configureStore';
+const store = configureStore();
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => 'Home!'} />
-          <Route
-            path="/hello"
-            render={() => <HelloWorld greeting="friend" />}
-          />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={'Home!'} />
+            <Route path="/hello" element={<HelloWorld greeting="friend" />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,62 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import PropTypes from 'prop-types';
+const GET_GREETINGS_REQUEST = 'GET_GREETINGS_REQUEST';
+const GET_GREETINGS_SUCCESS = 'GET_GREETINGS_SUCCESS';
+
+function getGreetings() {
+  console.log('getgreetings() Action!');
+  return (dispatch) => {
+    dispatch({ type: GET_GREETINGS_REQUEST });
+
+    return fetch(`v1/greetings.json`)
+      .then((response) => response.json())
+      .then((json) => dispatch(getGreetingsSuccess(json)))
+      .catch((error) => console.log(`Fetching Error ${error}`));
+  };
+}
+
+export function getGreetingsSuccess(json) {
+  return {
+    type: GET_GREETINGS_SUCCESS,
+    json,
+  };
+}
 class HelloWorld extends React.Component {
-  render () {
+  render() {
+    const { greetings } = this.props;
+    const greetingList = greetings.map((greeting) => {
+      return (
+        <li key={greeting.guid}>
+          {greeting.name} {greeting.guid}
+        </li>
+      );
+    });
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+        <button
+          className="getGreetingsbtn"
+          onClick={() => this.props.getGreetings()}
+        >
+          getGreetings
+        </button>
+        <br />
+        <ul>{greetingList} </ul>
       </React.Fragment>
     );
   }
 }
 
 HelloWorld.propTypes = {
-  greeting: PropTypes.string
+  greeting: PropTypes.string,
 };
-export default HelloWorld
+
+const structuredSelector = createStructuredSelector({
+  greetings: (state) => state.greetings,
+});
+
+const mapDispatchToProps = { getGreetings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
