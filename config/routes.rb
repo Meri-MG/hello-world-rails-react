c596813e9a3f767e965af24216032c12ac64b98e
Meri <mgogi13@freeuni.edu.ge>
2022-04-27 12:09:55 +0400

    feat: set greetings api routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static#index"
+  namespace :v1, defaults: {format: 'json'} do
+    get 'greetings', to: 'greetings#index'
+  end
 end
